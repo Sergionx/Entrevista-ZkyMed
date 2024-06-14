51d8c8eddae3fa9f7fcb52fbd708064e905e7e3a
Sergionx <sergio180203@gmail.com>
2024-06-13 23:06:03 -0400

    refactor: Update DeckModule to include Card entity
    This commit updates the DeckModule to include the Card entity. By adding the Card entity to the imports of the DeckModule, the codebase now has access to the Card entity and can perform operations related to cards within a deck. This change enhances the functionality of the DeckModule and improves the overall codebase by providing the necessary dependencies for managing cards.

--- server/src/deck/deck.module.ts
+++ server/src/deck/deck.module.ts
@@ -5,9 +5,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Deck } from './models/deck.entity';
 import { CardService } from './services/card.service';
 import { CardController } from './controllers/card.controller';
+import { Card } from './models/card.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Deck])],
+  imports: [TypeOrmModule.forFeature([Deck, Card])],
   providers: [DeckService, CardService],
   controllers: [DeckController, CardController],
 })
