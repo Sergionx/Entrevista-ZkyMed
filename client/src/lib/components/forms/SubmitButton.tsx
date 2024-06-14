21302dcf9df458f14d3c2832cd580ebcb00b03ad
Sergionx <sergio180203@gmail.com>
2024-06-13 23:06:50 -0400

    feat: Add SubmitButton component
    This commit adds the SubmitButton component to the codebase. The SubmitButton component is a reusable form button that handles form submission and displays a loading spinner when the form is being submitted. This addition enhances the user experience by providing a consistent and intuitive way to submit forms throughout the application.

---
+++ client/src/lib/components/forms/SubmitButton.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { cn } from "@/lib/utils/classNames";
+import { Button } from "@nextui-org/button";
+import { Spinner } from "@nextui-org/spinner";
+import React from "react";
+
+interface Props {
+  text?: string;
+  isLoading: boolean;
+  className?: string;
+}
+export default function SubmitButton({
+  text = "Crear",
+  isLoading,
+  className,
+}: Props) {
+  return (
+    <Button
+      type="submit"
+      color="primary"
+      className={cn(className)}
+      disabled={isLoading}
+    >
+      {isLoading && <Spinner color="default" size="sm" />}
+
+      {isLoading ? "Creating..." : text}
+    </Button>
+  );
+}
